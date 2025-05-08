987ee8de3563ec193b0b1d975b73abbcf9520298
hertas-collection <etaylor@bayescenter.com>
2025-05-08 12:53:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -232,4 +232,9 @@ if __name__ == '__main__':
 if __name__ == '__main__':
     from datetime import datetime
     import re
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+
+    # Get the port from the environment variable, or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host='0.0.0.0', port=port)
